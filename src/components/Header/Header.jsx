408ef22f29c8a305dfd8635fe746f6088c539ce0
jeyefendi <jeyhunfarajov@gmail.com>
2022-09-16 17:45:50 +0300

    mobile adaptive completed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,9 @@
-import styles from "./Header.module.css"
+import styles from "./Header.module.css";
 
 const Header = () => {
   return (
     <header>
         <div className={styles.logo}>
-            <img src="/src/assets/logo.svg" alt="Logo" />
         </div>
     </header>
   )
